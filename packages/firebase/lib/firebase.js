e989c8c5ad6e9472234ab0e5d44e75df806e365f
FazioNico <contact@nicolasfazio.ch>
2019-07-22 09:36:40 +0200

    init cmt

---
+++ packages/firebase/lib/firebase.js
@@ -0,0 +1,4 @@
+export var nm_test = function (arg) {
+    console.log((arg) ? "[TEST]: " + arg : '[TEST]: lib work!');
+};
+//# sourceMappingURL=firebase.js.map
---
+++ packages/firebase/src/firebase.ts
@@ -0,0 +1,3 @@
+export const nm_test = (arg: string) => {
+  console.log((arg) ? `[TEST]: ${arg}`: '[TEST]: lib work!');
+}
