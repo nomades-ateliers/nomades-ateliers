b14b654a10ecc8e59aad54e56911295bc7a4032d
FazioNico <contact@nicolasfazio.ch>
2019-07-22 10:03:32 +0200

    upd test publish

--- packages/firebase/lib/firebase.js
+++ packages/firebase/lib/firebase.js
@@ -1,4 +1,5 @@
 export var nm_test = function (arg) {
+    // return msg
     console.log((arg) ? "[TEST]: " + arg : '[TEST]: lib work!');
 };
 //# sourceMappingURL=firebase.js.map
--- packages/firebase/src/firebase.ts
+++ packages/firebase/src/firebase.ts
@@ -1,3 +1,4 @@
 export const nm_test = (arg: string) => {
+  // return msg
   console.log((arg) ? `[TEST]: ${arg}`: '[TEST]: lib work!');
 }
